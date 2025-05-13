9a56766d3a446b1346ea8b4af538c5b0a6c5555f
John Mark Managbanag <jmmanagbanag123@gmail.com>
2025-05-13 19:57:46 +0800

    fix make_fake_person function

--- pentalpha/managbanag.py
+++ pentalpha/managbanag.py
@@ -1,16 +1,22 @@
 from faker import Faker
 
 def make_fake_person():
-    """Generate a fake person profile using the Faker library."""
     fake = Faker()
+    user_input = int(input("How many fake profiles do you like to generate? "))
 
-    person_info = {
-        "Name": fake.name(),
-        "Birthday": fake.date_of_birth(minimum_age=18, maximum_age=90), 
-        "Contact Number": fake.phone_number(),
-        "Email": fake.email(),
-        "Address": fake.address(), 
-        "Job": fake.job(),
-        "Company": fake.company()
-    }
-    return person_info
+    for count in range(user_input):
+        print("")
+        print(f"Profile #{count + 1}:")
+        
+        person_info = {
+            "Name": fake.name(),
+            "Birthday": fake.date_of_birth(minimum_age=18, maximum_age=90),
+            "Contact Number": fake.phone_number(),
+            "Email": fake.email(),
+            "Address": fake.address(),
+            "Job": fake.job(),
+            "Company": fake.company()
+        }
+        
+        for title, info in person_info.items():
+            print(f"{title}: {info}")
