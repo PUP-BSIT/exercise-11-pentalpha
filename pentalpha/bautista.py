211b793462ad1a190547b7841b067dcc2c1c4fbf
bautista-johnmatthew <johnmatthewbautista01@gmail.com>
2025-05-13 19:16:39 +0800

    change get_constellation parameters

--- pentalpha/bautista.py
+++ pentalpha/bautista.py
@@ -2,14 +2,15 @@ from ephem import constellation, stars
 from datetime import date
 from random import choice
 
-def get_constellation(star_name = "", date = date.today()):
+def get_constellation(star_name = ""):
     """ Prints the constellation the given name of a star belongs to """
     if star_name == "":
         star_name = choice(stars.STAR_NUMBER_NAME)
 
     # Observe with the default date
+    observing_date = date.today()
     object = stars.star(star_name.title())
-    object.compute(date)
+    object.compute(observing_date)
     constellation_result = constellation(object)[1]
     
     print(f"The star {star_name} is part of the " 
