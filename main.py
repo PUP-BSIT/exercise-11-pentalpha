0c15287de450c90f2607ecdeaecce4a895054792
bautista-johnmatthew <johnmatthewbautista01@gmail.com>
2025-05-13 19:26:41 +0800

    add main.py

---
+++ main.py
@@ -0,0 +1,9 @@
+# TODO: (Bautista) Call get_constellation function
+
+# TODO: (Banzali) Call hash_text function
+
+# TODO: (Raymundo) Call convert_grayscale function
+
+# TODO: (Managbanag) Call make_fake_person function
+
+# TODO: (Espinola) Call jokes function
